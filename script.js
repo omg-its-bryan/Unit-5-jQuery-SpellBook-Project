9d055b2f8ae3c1c1df631427d83070fca943be6c
omg-its-bryan <43577131+omg-its-bryan@users.noreply.github.com>
2019-03-05 09:32:41 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".Disappear").click(function(){
+ $(".Dove").fadeOut();
+});
+
+
+
+//Spell #2
+
+$(".Away").click(function(){
+ $(".coin").toggle();
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
